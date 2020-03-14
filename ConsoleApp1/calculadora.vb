67b9e11e9368e37f8fccf4ccbeb47ff9ba3c1f9b
FLIA <FLIA@PCVOX>
2020-03-14 13:27:51 -0300

    .

---
+++ ConsoleApp1/calculadora.vb
@@ -0,0 +1,15 @@
+﻿Class Calculadora
+    Private num1 As Integer
+    Private num2 As Integer
+
+    Public Sub New(num1 As Integer, num2 As Integer)
+        Me.num1 = num1
+        Me.num2 = num2
+    End Sub
+
+    Public Function sumar(num1 As Integer, num2 As Integer)
+
+    End Function
+
+
+End Class
